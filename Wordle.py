242e5f970fe96bc817ec4be3a8e7e47ef0c8c4a1
hunterb8 <brittan.hunter@gmail.com>
2022-10-01 17:11:52 -0600

    Milestone #1 complete
    added code to randomly select word from dictionary and fill in first line with letters

--- Wordle.py
+++ Wordle.py
@@ -1,22 +1,25 @@
 # File: Wordle.py
 
-"""
-This module is the starter file for the Wordle assignment.
-BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
-"""
-
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-
     def enter_action(s):
         gw.show_message("You have to implement this method.")
+        
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+    #Selects random word from dictionary
+    word = FIVE_LETTER_WORDS[random.randint(0,(len(FIVE_LETTER_WORDS)-1))].upper()
+
+    letterNum = 0
+    for letter in word:
+        gw.set_square_letter(0,letterNum,letter)
+        letterNum += 1 
 
 # Startup code
 
