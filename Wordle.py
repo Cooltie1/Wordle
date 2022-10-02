77b823febdd92e9990be26dadd26c20ef1ccc20b
hunterb8 <brittan.hunter@gmail.com>
2022-10-01 22:55:05 -0600

    Milestone #2 complete
    added to enter_action method to make it so the user's entered word is check against the dictionary. it's printed out in a message if it's in the dictionary. if not, it's cleared and the user is notified that it's not in the dictionary.

--- Wordle.py
+++ Wordle.py
@@ -7,7 +7,16 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        currentRow = gw.get_current_row()
+        buildWord = ""
+        for tile in range(0,N_COLS):
+            buildWord += gw.get_square_letter(currentRow,tile)
+        if buildWord.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("You guessed " + buildWord)
+        else:
+            for tile in range(0,N_COLS):
+                gw.set_square_letter(currentRow,tile,"")
+            gw.show_message("Not in word list.")
         
 
     gw = WordleGWindow()
@@ -19,7 +28,12 @@ def wordle():
     letterNum = 0
     for letter in word:
         gw.set_square_letter(0,letterNum,letter)
-        letterNum += 1 
+        letterNum += 1
+
+    gw.set_current_row(1)
+
+
+
 
 # Startup code
 
