679c8c642f05c4309ce6c9655413ddd11ce29527
Scott Olson <scottolsonofficial@gmail.com>
2022-10-08 11:47:59 -0600

    end game pt2

--- WordleGraphics.py
+++ WordleGraphics.py
@@ -96,29 +96,30 @@ class WordleGWindow:
                                  MESSAGE_Y)
 
         def key_action(tke):
-            if isinstance(tke, str):
-                ch = tke.upper()
-            else:
-                ch = tke.char.upper()
-            if ch == "\007" or ch == "\177" or ch =="\010" or ch == "DELETE":
-                self.show_message("")
-                if self._row < N_ROWS and self._col > 0:
-                    self._col -= 1
-                    sq = self._grid[self._row][self._col]
-                    sq.set_letter(" ")
-            elif ch == "\r" or ch == "\n" or ch == "ENTER":
-                self.show_message("")
-                s = ""
-                for col in range(N_COLS):
-                    s += self._grid[self._row][col].get_letter();
-                for fn in self._enter_listeners:
-                    fn(s)
-            elif ch.isalpha():
-                self.show_message("")
-                if self._row < N_ROWS and self._col < N_COLS:
-                    sq = self._grid[self._row][self._col]
-                    sq.set_letter(ch)
-                    self._col += 1
+            if not self.win:
+                if isinstance(tke, str):
+                    ch = tke.upper()
+                else:
+                    ch = tke.char.upper()
+                if ch == "\007" or ch == "\177" or ch =="\010" or ch == "DELETE":
+                    self.show_message("")
+                    if self._row < N_ROWS and self._col > 0:
+                        self._col -= 1
+                        sq = self._grid[self._row][self._col]
+                        sq.set_letter(" ")
+                elif ch == "\r" or ch == "\n" or ch == "ENTER":
+                    self.show_message("")
+                    s = ""
+                    for col in range(N_COLS):
+                        s += self._grid[self._row][col].get_letter();
+                    for fn in self._enter_listeners:
+                        fn(s)
+                elif ch.isalpha():
+                    self.show_message("")
+                    if self._row < N_ROWS and self._col < N_COLS:
+                        sq = self._grid[self._row][self._col]
+                        sq.set_letter(ch)
+                        self._col += 1
 
         def press_action(tke):
             self._down_x = tke.x
